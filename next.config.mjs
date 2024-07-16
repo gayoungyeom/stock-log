13ad63829fb9d751629ccc084fce8766a211097c
gayoung <gayoungyeom@gmail.com>
2024-07-16 23:45:23 +0900

    feat: gh pages 배포 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://gayoungyeom.github.io/stock-log" : "",
+}
 
 export default nextConfig
