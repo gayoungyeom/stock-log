6df0ed2efbed55efbfe81a81c4b346c348c04688
gayoung <gayoungyeom@gmail.com>
2025-08-04 01:08:35 +0900

    fix: github-pages에서 vercel로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  trailingSlash: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://gayoungyeom.github.io/stock-log" : "",
-}
+const nextConfig = {}
 
 export default nextConfig
